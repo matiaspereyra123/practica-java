48aa869bc0e4ed4a4fb19cd228db5f82c7159696
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2023-12-18 19:59:38 -0300

    Revert "Practicando herencias y matrices"
    This reverts commit 41ee7a2c55607c051d77f98af8f7a850914401f1.

--- src/operaciones/Matrices.java
+++ src/operaciones/Matrices.java
@@ -4,30 +4,7 @@ package operaciones;
 
 public class Matrices {
     public static void main(String[] args) {
-            int edades[][] = new int [3][2];
-            edades[0][0]=5;
-            edades[0][1]=7;
-            edades[1][0]=8;
-            edades[1][1]=4;
-            
-            System.out.println("edad [0]-[0] :"+edades[0][0]);
-            for (int ren = 0; ren < edades.length; ren++) {
-                for (int col = 0; col < edades[ren].length; col++) {
-                    System.out.println("Edades:"+ren+"-"+col+":"+edades[ren][col]);
-                }
-            
-        }
-            System.out.println("--");
-            
-            String frutas[][]={{"Naranja", "Limon"},{"Fresa", "Zarzamora","Mora"}};
-            for (int ren = 0; ren < frutas.length; ren++) {
-                for (int col = 0; col < frutas[ren].length; col++) {
-                     System.out.println("Frutas:"+ren+"-"+col+":"+frutas[ren][col]);
-                }
-        }
-            
-            
-                int array[][] = {
+            int array[][] = {
                                 {1,2,3,4,5},
                                 {6,7,8,9,10},
                                 {11,12,13,14,15}
