2c4afee257bb85bcabbcb23f7cfdc11b51c6ac38
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2023-12-18 19:53:21 -0300

    Practicando herencias y matrices

--- src/test/TestHerencia.java
+++ src/test/TestHerencia.java
@@ -3,6 +3,7 @@ package test;
 
 import domain.Cliente;
 import domain.Empleado;
+import domain.Persona;
 import java.util.Date;
 
 
@@ -18,6 +19,21 @@ public class TestHerencia {
     
          Cliente cliente2 = new Cliente(fecha, false, "Milo",   'M', 10, "Goleta Fortuna 5951");
          System.out.println("cliente2 = " + cliente2);
-    }
+         
+         //Practica matrices
+         Persona personas[][]=new Persona[2][3];
+            personas[0][0]= new Persona("Juan");
+            personas[0][1]= new Persona("Karla");
+            imprimir(personas);
+     }
    
+        
+            public static void imprimir(Object matriz[][]){
+                      for (int ren = 0; ren < matriz.length; ren++) {
+                for (int col = 0; col < matriz[ren].length; col++) {
+                     System.out.println("Matriz:"+ren+"-"+col+":"+matriz[ren][col]);
+                }
+            }
+            }
+        
 }
